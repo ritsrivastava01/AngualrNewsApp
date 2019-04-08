46bc1defb5ec7031b322257f1379d866e8ed47a2
Ritz <ritsrivastava@gmail.com>
2019-04-08 13:02:43 +0200

    more components and store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
+import { NewsItemComponent } from './news/news-item/news-item.component';
+import { SectionsComponent } from './sections/sections.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent,
+    NewsItemComponent,
+    SectionsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers })
   ],
   providers: [],
   bootstrap: [AppComponent]
